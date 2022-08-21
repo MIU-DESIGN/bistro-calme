26d2a484951fe029f17bbf985f9eb248aa8f10ea
yumsn <gin728emma218@gmail.com>
2022-08-21 16:24:09 +0900

    4-02記事一覧をカスタマイズする

--- functions.php
+++ functions.php
@@ -50,3 +50,20 @@ function my_comment_form_default_fields($args){
   return $args;
 }
 
+/**
+ * 記事一覧をカスタマイズするアクションフック
+ */
+add_action( 'pre_get_posts', 'my_pre_get_posts');
+function my_pre_get_posts($query){
+  // 管理画面、メインクエリ以外には設定しない
+  if(is_admin() || !$query->is_main_query() ){
+    return;
+  }
+  
+  //トップページ の 場合
+  if($query->is_home()){
+    $query->set('posts_per_page', 3);
+    // $query->set('category_name', 'news');
+    return;
+  }
+}
